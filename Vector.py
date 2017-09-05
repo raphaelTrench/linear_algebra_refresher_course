cb538ce0e8f69143ecb06667ecd950cf130debae
raphael <raphaeltrench@gmail.com>
2017-09-05 01:28:23 -0300

    cross product

--- Vector.py
+++ Vector.py
@@ -7,7 +7,7 @@ class Vector(object):
 		try:
 			if not coordinates:
 				raise ValueError
-			self.coordinates = tuple(Decimal(x) for x in coordinates)
+			self.coordinates = tuple([Decimal(x) for x in coordinates])
 			self.dimension = len(coordinates)
 
 		except ValueError:
@@ -26,11 +26,11 @@ class Vector(object):
 
 	def plus(self,v):
 		new_coordinates = [x+y for x,y in zip(self.coordinates, v.coordinates)]
-		return Vector(new_coordenates)
+		return Vector(new_coordinates)
 
 	def minus(self,v):
 		new_coordinates = [x-y for x,y in zip(self.coordinates, v.coordinates)]
-		return Vector(new_coordenates)
+		return Vector(new_coordinates)
 
 	def times_scalar(self,scalar):
 		new_coordinates = [x * Decimal(scalar) for x in self.coordinates]
@@ -45,28 +45,101 @@ class Vector(object):
 
 	def normalize(self):
 		try:
-			return self.times_scalar(Decimal('1.0')/magnitude)
+			magnitude = self.magnitude()
+			return self.times_scalar(Decimal('1.0')/Decimal(magnitude))
 		except ZeroDivisionError:
-			raise Exception('Cannot normalize zero vector')
+			raise Exception(self.CANNOT_NORMALIZE_ZERO_VECTOR_MSG)
 
 	def dot_product(self, v):
-		acc = 0
-		for i in range(len(self.coordinates)):
-			acc = acc + self.coordinates[i] * v.coordinates[i]
-		return acc
+		return sum([x*y for x,y in zip(self.coordinates, v.coordinates)])
 
-	def angle(self, v,degrees=True):
+	def angle(self, v,degrees=False):
 		try:
-			from math import acos, pi
-			if degrees:
-				degrees = 180 / pi
-				return acos(self.dot_product(v)/(self.magnitude() * v.magnitude())) * degrees
+			from math import pi,acos
+			u = self.normalize()
+			w = v.normalize()
+			radians = acos(round(u.dot_product(w),10))
+
+			if(degrees):
+				degrees_per_radians = 180. / pi
+				return radians * degrees_per_radians
 			else:
-				return acos(self.dot_product(v)/(self.magnitude() * v.magnitude()))
+				return radians
 
-		except ZeroDivisionError:
-			raise Exception('Cannot compute an angle with a zero vector')
+		except Exception as e:
+			if str(e) == self.CANNOT_NORMALIZE_ZERO_VECTOR_MSG:
+				raise Exception('Cannot compute an angle with the zero vector')
 
-v = Vector([7.35,0.221,5.188])
-w = Vector([2.751,8.259,3.985])
-print (v.angle(w))
+	def isZeroVector(self):
+		tolerance = 1e-10
+		return (self.magnitude() < tolerance)
+
+	def isParallel(self, v):
+		if(self.isZeroVector() or v.isZeroVector()):
+			return True
+		scalar = self.coordinates[0] / v.coordinates[0]
+		i = 1
+		for i in range(len(self.coordinates)):
+			if((round(scalar,10) != round(self.coordinates[i]/v.coordinates[i],13))):
+				return False
+		return True
+
+	def isOrthogonal(self,v):
+		tolerance = 1e-10
+		return abs(self.dot_product(v)) < tolerance
+
+	def component_parallelTo(self,basis):
+		try:
+			b = basis.normalize();
+			dot = self.dot_product(b)
+			return b.times_scalar(dot)
+		except Exception as e:
+			if str(e) == self.CANNOT_NORMALIZE_ZERO_VECTOR_MSG:
+				raise Exception(self.NO_UNIQUE_PARALLEL_COMPONENT_MSG)
+			else:
+				raise e
+
+	def component_orthogonalTo(self,basis):
+		try:
+			return self.minus(self.projectionOn(basis))
+		except Exception as e:
+			if str(e) == self.NO_UNIQUE_PARALLEL_COMPONENT_MSG:
+				raise Exception(self.NO_UNIQUE_ORTHOGONAL_COMPONENT_MSG)
+			else:
+				raise e
+
+	def crossProduct(self,v):
+		try:
+			from math import pi
+			new_coordinates = []
+			if(self.isZeroVector() or v.isZeroVector()):
+				new_coordinates.append(0)
+				new_coordinates.append(0)
+				new_coordinates.append(0)		
+			else:		
+				new_coordinates.append((self.coordinates[1]*v.coordinates[2]) - (v.coordinates[1]*self.coordinates[2]))
+				new_coordinates.append(-((self.coordinates[0]*v.coordinates[2]) - (v.coordinates[0]*self.coordinates[2])))
+				new_coordinates.append((self.coordinates[0]*v.coordinates[1]) - (v.coordinates[0]*self.coordinates[1]))
+			return Vector(new_coordinates)
+		except ValueError as e:
+			msg = str(e)
+			if(msg == 'need more than 2 values to unpack'):
+				self_embedded_in_R3 = Vector(self.coordinates + ('0',))
+				v_embedded_in_R3 = Vector(v.coordinates + ('0',))
+				return self_embedded_in_R3.crossProduct(v_embedded_in_R3)
+			elif(msg == 'too many values to unpack' or msg == 'need more than 1 value to unpack'):
+				raise Exception(self.ONLY_DEFINED_IN_TWO_THREE_DIMS_MSG)
+			else:
+				raise e
+
+	def parallelogramArea(self,v):
+		vector = self.crossProduct(v)
+		return vector.magnitude()
+
+	def triangleArea(self,v):
+		vector = self.crossProduct(v)
+		return 0.5 * vector.magnitude()
+
+v = Vector([1.5,9.547,3.691])
+w = Vector([-6.007,0.124,5.772])
+print(v.triangleArea(w))
