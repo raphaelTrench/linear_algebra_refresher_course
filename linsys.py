26060cae421fff30a07c25ebe6c198ffc86bb53d
raphael <raphaeltrench@gmail.com>
2017-10-02 00:59:40 -0300

    GE Solution

--- linsys.py
+++ linsys.py
@@ -77,6 +77,45 @@ class LinearSystem(object):
             tf.clearCoefficientsAbove(i, j)
         return tf
 
+    def compute_solution(self):
+        try:
+            return self.GE_Solution()
+        except Exception as e:
+            if(str(e) == self.NO_SOLUTIONS_MSG or str(e) == self.INF_SOLUTIONS_MSG):
+                return str(e)
+            else:
+                raise e  
+    
+    def raise_exception_if_contradictory_equation(self):
+        for p in self.planes:
+            try:
+                p.first_nonzero_index(p.normal_vector)
+            except Exception as e:
+                if str(e) == 'No nonzero elements found':
+                    constant_term = MyDecimal(p.constant_term)
+                    if not(constant_term.is_near_zero()):
+                        raise Exception(self.NO_SOLUTIONS_MSG)
+                else:
+                    raise e
+
+    def raise_exception_if_too_few_pivots(self):
+        pivot_indices = self.indices_of_first_nonzero_terms_in_each_row()
+        num_pivots = sum([1 if index >= 0 else 0 for index in pivot_indices])
+        num_variables = self.dimension
+
+        if(num_pivots < num_variables):
+           raise Exception(self.INF_SOLUTIONS_MSG)
+
+    def GE_Solution(self):
+        rref = self.compute_rref()
+        rref.raise_exception_if_contradictory_equation()
+        rref.raise_exception_if_too_few_pivots()
+
+        num_variables = rref.dimension
+        solution_coordinates = [rref.planes[i].constant_term for i in range(num_variables)]
+        return Vector(solution_coordinates);
+
+    
     def scaleRowToMakeCoefficientOne(self,row,col):
         n = self[row].normal_vector
         beta = Decimal('1.0') / n[col]
@@ -154,40 +193,20 @@ class MyDecimal(Decimal):
     def is_near_zero(self, eps=1e-10):
         return abs(self) < eps
 
-p1 = Plane(normal_vector=Vector(['1','1','1']), constant_term='1')
-p2 = Plane(normal_vector=Vector(['0','1','1']), constant_term='2')
+p1 = Plane(normal_vector=Vector(['5.862','1.178','-10.366']), constant_term='-8.15')
+p2 = Plane(normal_vector=Vector(['-2.931','-0.589','5.183']), constant_term='-4.075')
 s = LinearSystem([p1,p2])
-r = s.compute_rref()
-if not (r[0] == Plane(normal_vector=Vector(['1','0','0']), constant_term='-1') and
-        r[1] == p2):
-    print ('test case 1 failed')
+print(s.GE_Solution())
 
-p1 = Plane(normal_vector=Vector(['1','1','1']), constant_term='1')
-p2 = Plane(normal_vector=Vector(['1','1','1']), constant_term='2')
-s = LinearSystem([p1,p2])
-r = s.compute_rref()
-if not (r[0] == p1 and
-        r[1] == Plane(constant_term='1')):
-    print ('test case 2 failed')
-
-p1 = Plane(normal_vector=Vector(['1','1','1']), constant_term='1')
-p2 = Plane(normal_vector=Vector(['0','1','0']), constant_term='2')
-p3 = Plane(normal_vector=Vector(['1','1','-1']), constant_term='3')
-p4 = Plane(normal_vector=Vector(['1','0','-2']), constant_term='2')
-s = LinearSystem([p1,p2,p3,p4])
-r = s.compute_rref()
-if not (r[0] == Plane(normal_vector=Vector(['1','0','0']), constant_term='0') and
-        r[1] == p2 and
-        r[2] == Plane(normal_vector=Vector(['0','0','-2']), constant_term='2') and
-        r[3] == Plane()):
-    print ('test case 3 failed')
-
-p1 = Plane(normal_vector=Vector(['0','1','1']), constant_term='1')
-p2 = Plane(normal_vector=Vector(['1','-1','1']), constant_term='2')
-p3 = Plane(normal_vector=Vector(['1','2','-5']), constant_term='3')
+p1 = Plane(normal_vector=Vector(['8.632','5.112','-1.186']), constant_term='-5.113')
+p2 = Plane(normal_vector=Vector(['4.315','11.132','-5.27']), constant_term='-6.775')
+p3 = Plane(normal_vector=Vector(['-2.158','3.01','-1.727']), constant_term='-0.831')
 s = LinearSystem([p1,p2,p3])
-r = s.compute_rref()
-if not (r[0] == Plane(normal_vector=Vector(['1','0','0']), constant_term=Decimal('23')/Decimal('9')) and
-        r[1] == Plane(normal_vector=Vector(['0','1','0']), constant_term=Decimal('7')/Decimal('9')) and
-        r[2] == Plane(normal_vector=Vector(['0','0','1']), constant_term=Decimal('2')/Decimal('9'))):
-    print ('test case 4 failed')
+print(s.GE_Solution())
+
+p1 = Plane(normal_vector=Vector(['5.262','2.739','-9.878']), constant_term='-3.441')
+p2 = Plane(normal_vector=Vector(['5.111','6.358','7.638']), constant_term='-2.152')
+p3 = Plane(normal_vector=Vector(['2.016','-9.924','-1.367']), constant_term='-9.278')
+p4 = Plane(normal_vector=Vector(['2.167','-13.543','-18.883']), constant_term='-10.567')
+s = LinearSystem([p1,p2,p3,p4])
+print(s.GE_Solution())
