e08d20797bf6aafd4be20c6efa12ecf99f0e3156
raphael <raphaeltrench@gmail.com>
2017-10-08 01:28:16 -0300

    hyperplane.py

--- linsys.py
+++ linsys.py
@@ -2,7 +2,7 @@ from decimal import Decimal, getcontext
 from copy import deepcopy
 
 from vector import Vector
-from plane import Plane
+from hyperplane import Hyperplane
 
 getcontext().prec = 30
 
@@ -35,7 +35,7 @@ class LinearSystem(object):
             try:
                 indices[i] = p.first_nonzero_index(p.normal_vector)
             except Exception as e:
-                if str(e) == Plane.NO_NONZERO_ELTS_FOUND_MSG:
+                if str(e) == Hyperplane.NO_NONZERO_ELTS_FOUND_MSG:
                     continue
                 else:
                     raise e
@@ -81,7 +81,7 @@ class LinearSystem(object):
         try:
             return self.GE_Solution()
         except Exception as e:
-            if(str(e) == self.NO_SOLUTIONS_MSG or str(e) == self.INF_SOLUTIONS_MSG):
+            if(str(e) == self.NO_SOLUTIONS_MSG):
                 return str(e)
             else:
                 raise e  
@@ -109,13 +109,45 @@ class LinearSystem(object):
     def GE_Solution(self):
         rref = self.compute_rref()
         rref.raise_exception_if_contradictory_equation()
-        rref.raise_exception_if_too_few_pivots()
 
-        num_variables = rref.dimension
-        solution_coordinates = [rref.planes[i].constant_term for i in range(num_variables)]
-        return Vector(solution_coordinates);
+        direction_vectors = rref.extract_vectors_for_parametrization()
+        basepoint = rref.extract_basepoint_for_parametrization()
+        return Parametrization(basepoint,direction_vectors);
 
-    
+    def extract_vectors_for_parametrization(self):
+        num_variables = self.dimension
+        pivot_indices = self.indices_of_first_nonzero_terms_in_each_row()
+        free_var_indices = set(range(num_variables)) - set(pivot_indices)
+
+        direction_vectors = []
+
+        for free_var in free_var_indices:
+            vector_coords = [0] * num_variables
+            vector_coords[free_var] = 1
+            for i,p in enumerate(self.planes):
+                pivot_var = pivot_indices[i]
+                if(pivot_var < 0):
+                    break
+                vector_coords[pivot_var] = -p.normal_vector[free_var]
+            direction_vectors.append(Vector(vector_coords))
+
+        return direction_vectors
+
+
+    def extract_basepoint_for_parametrization(self):
+        num_variables = self.dimension
+        pivot_indices = self.indices_of_first_nonzero_terms_in_each_row()
+
+        basepoint_coords = [0] * num_variables
+
+        for i,p in enumerate(self.planes):
+            pivot_var = pivot_indices[i]
+            if(pivot_var<0):
+                break
+            basepoint_coords[pivot_var] = p.constant_term
+
+        return Vector(basepoint_coords)
+
     def scaleRowToMakeCoefficientOne(self,row,col):
         n = self[row].normal_vector
         beta = Decimal('1.0') / n[col]
@@ -153,7 +185,7 @@ class LinearSystem(object):
     def multiply_coefficient_and_row(self, coefficient, row):
         aux = self[row].normal_vector.times_scalar(coefficient)
         aux2 = self[row].constant_term * coefficient
-        self[row] = Plane(normal_vector=aux, constant_term=aux2)
+        self[row] = Hyperplane(normal_vector=aux, constant_term=aux2)
 
     def add_multiple_times_row_to_row(self, coefficient, row_to_add, row_to_be_added_to):
         row1 = self[row_to_add].normal_vector.times_scalar(coefficient)
@@ -161,7 +193,7 @@ class LinearSystem(object):
         row2 = self[row_to_be_added_to].normal_vector.plus(row1)
         k2 = self[row_to_be_added_to].constant_term
         k = k1 + k2
-        self[row_to_be_added_to] = Plane(normal_vector=row2,constant_term=k)        
+        self[row_to_be_added_to] = Hyperplane(normal_vector=row2,constant_term=k)        
 
    
 
@@ -193,20 +225,43 @@ class MyDecimal(Decimal):
     def is_near_zero(self, eps=1e-10):
         return abs(self) < eps
 
-p1 = Plane(normal_vector=Vector(['5.862','1.178','-10.366']), constant_term='-8.15')
-p2 = Plane(normal_vector=Vector(['-2.931','-0.589','5.183']), constant_term='-4.075')
-s = LinearSystem([p1,p2])
-print(s.GE_Solution())
+class Parametrization(object):
 
-p1 = Plane(normal_vector=Vector(['8.632','5.112','-1.186']), constant_term='-5.113')
-p2 = Plane(normal_vector=Vector(['4.315','11.132','-5.27']), constant_term='-6.775')
-p3 = Plane(normal_vector=Vector(['-2.158','3.01','-1.727']), constant_term='-0.831')
-s = LinearSystem([p1,p2,p3])
-print(s.GE_Solution())
+    BASEPT_AND_DIR_VECTORS_MUST_BE_IN_SAME_DIM = (
+        'The basepoint and direction vectors should all live in the same '
+        'dimension')
 
-p1 = Plane(normal_vector=Vector(['5.262','2.739','-9.878']), constant_term='-3.441')
-p2 = Plane(normal_vector=Vector(['5.111','6.358','7.638']), constant_term='-2.152')
-p3 = Plane(normal_vector=Vector(['2.016','-9.924','-1.367']), constant_term='-9.278')
-p4 = Plane(normal_vector=Vector(['2.167','-13.543','-18.883']), constant_term='-10.567')
-s = LinearSystem([p1,p2,p3,p4])
-print(s.GE_Solution())
+    def __init__(self, basepoint, direction_vectors):
+
+        self.basepoint = basepoint
+        self.direction_vectors = direction_vectors
+        self.dimension = self.basepoint.dimension
+
+        try:
+            for v in direction_vectors:
+                assert v.dimension == self.dimension
+
+        except AssertionError:
+            raise Exception(self.BASEPT_AND_DIR_VECTORS_MUST_BE_IN_SAME_DIM)
+
+    def __str__(self):
+
+        output = ''
+        for coord in range(self.dimension):
+            output += 'x_{} = {} '.format(coord + 1,
+                                          round(self.basepoint[coord], 3))
+            for free_var, vector in enumerate(self.direction_vectors):
+                output += '+ {} t_{}'.format(round(vector[coord], 3),
+                                             free_var + 1)
+            output += '\n'
+        return output
+
+p1 = Hyperplane(normal_vector=Vector(['0.786', '0.786', '8.123', '1.111', '-8.363']),
+                constant_term='-9.955')
+p2 = Hyperplane(normal_vector=Vector(['0.131', '-0.131', '7.05', '-2.813', '1.19']),
+                constant_term='-1.991')
+p3 = Hyperplane(normal_vector=Vector(['9.015', '-5.873', '-1.105', '2.013', '-2.802']),
+                constant_term='-3.982')
+
+system = LinearSystem([p1, p2, p3])
+print (system.compute_solution())
